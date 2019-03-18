7c2a2eca4ca2f028ecc7c9a3036a02e55e4238d4
citron0137 <37734231+citron0137@users.noreply.github.com>
2019-03-19 01:02:55 +0900

    Update recv.py

--- recv.py
+++ recv.py
@@ -1,11 +1,22 @@
 import socket
-
+import json
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind(('127.0.0.1',1234))
 
 
+def getJsonFromUrl(urlString):
+    #print(urlString)
+    response = urllib.request.urlopen(urlString).read().decode()
+    responseJson = json.loads(response)
+    return responseJson
+
+
 while 1:
     data, addr = sock.recvfrom(0xffff)
-    print("server is received data:", data.decode())
-    print("Send Client IP : ", addr[0])
-    print("Send Client Port :", addr[1])
+    print("server is received data")
+    comments = json.loads(data)
+    print(json.dumps(comments, indent=2, sort_keys=True))
+    for comment in comments["data"]:
+        print("message : " + comment["message"])
+        print("writer : " + comment["writer"])
+
