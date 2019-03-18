6b751013e6c8de142652007d248e21a97a6cd324
citron0137 <37734231+citron0137@users.noreply.github.com>
2019-03-18 22:47:54 +0900

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -5,6 +5,9 @@ import datetime as DT
 import socket
 access_token="EAAE6dVYNwNIBAA5jsqhQtHsh9fy2AU2StYHJ2kpgzPPkjijbcN6ZAgeh6enyYUNhAKI7mIZBTt63TEqC7xI6f0YiR8UGZCaKEeWW6OPD4ZBRpPcF7VkMsxgvKjJ6Krsrkgk9kn9YSCgsFjmMsSjRBiZCm97NkU5eOZAe4Ar9cnv0vNIAXLzALEMdJZAku67qCGsSwLXnJ12D4rIOMQzgi3f"
 access_token="EAAE6dVYNwNIBAA4gl3LUkPsAjOLMB75YxHuYDbbZBOjkkoYauEnFtBOktw9IDqNOCN5x6dSoU8TOehNW9ZA0FZC5ckwnnpZAnuZAmCbT0fmba52YfoPN1qPjXISDr9ASIwI5rj1ZBW2plbmIIznjYDtKdK4m0knJPLn7JfoGTD9ZC5VWDEUGWVQIodETZAAR0RhNAZAyM6CdZBrj2cD9muJOEw"
+
+access_token="EAAE6dVYNwNIBABzHnc6DosQDrK4BbRFgZBqugI3Mt1PpqybjhbNHfZBBZCS8YKDd6gcZAQdV5oFTmEabL51wKx0BdSzeV2NQZA6oZCKAuXs7tZAQoCFMK5JGj93lMiAehSDk4NexMZCxNdXxKcGE4Df2ObhFZBtGdWJV2akYMZChNA7nU5AbA5V72pWjq7l8mhjHXUCUJj43UNTYPduurniNZAL"
+
 tokenParam = "access_token="+access_token
 
 
@@ -43,50 +46,57 @@ def get_comments(post_id):
         '''get Likes
         '''
         likeUrl="https://graph.facebook.com/v3.2/"+comment["id"]+"/reactions?"+tokenParam
-        likeJson = getJsonFromUrl(likeUrl)
+        #likeJson = getJsonFromUrl(likeUrl)
         #print(likeJson["data"])
-        print("like count :",len(likeJson["data"]))
+        #print("like count :",len(likeJson["data"]))
         print("--------------------------")        
         resultComment = comment
         #resultComment["writer"] = comment["from"]["name"]
-        resultComment["like_count"] = len(likeJson["data"])
-
+        #resultComment["like_count"] = len(likeJson["data"])
+        resultComment["like_count"]=0
         #print(get_comments(comment["id"]))
-        result += get_comments(comment["id"])
+        #result += get_comments(comment["id"])
         #del resultComment["from"]
         del resultComment["id"]
         result.append(resultComment)
     return result
 
 
-get_posts()
+#get_posts()
 post_num = "264581367591821_264581670925124"
+post_num = "264581367591821_264973414219283"
 while True:
-    all_comments = get_comments(post_num)
-    filtered_comments=[]
-    this_time = datetime.now(DT.timezone.utc)
-    for comment in all_comments:
-        that_time = datetime.strptime(comment["created_time"], "%Y-%m-%dT%H:%M:%S%z")
-        print("--------------------")
-        print("this time : ", this_time)
-        print("that time : ", that_time)
-        print("time delta : ", this_time-that_time)
-        print("--------------------")
-        print(comment["message"])
-        if(len(comment["message"]) > 20):
-            continue
-        if(this_time-that_time <  DT.timedelta(seconds=60*10) or int(comment["like_count"])>=10):
-        #if(True):
-            print(comment)
-            filtered_comments.append(comment)
-    print("================================================")
-    print(json.dumps(filtered_comments,indent=2, sort_keys=True))
-    print("===============================================")
-    filteredJson={}
-    filteredJson["data"] = filtered_comments
-    sendData= json.dumps(filteredJson,indent=2, sort_keys=True)
-    byteData = bytes(sendData, 'utf-8')
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.sendto(byteData,("127.0.0.1",1234))
-    break
-
+    start_time = datetime.now(DT.timezone.utc)
+    try:
+        all_comments = get_comments(post_num)
+        filtered_comments=[]
+        this_time = datetime.now(DT.timezone.utc)
+        for comment in all_comments:
+            that_time = datetime.strptime(comment["created_time"], "%Y-%m-%dT%H:%M:%S%z")
+            print("--------------------")
+            print("this time : ", this_time)
+            print("that time : ", that_time)
+            print("time delta : ", this_time-that_time)
+            print("--------------------")
+            print(comment["message"])
+            if(len(comment["message"]) > 20):
+                continue
+            if(this_time-that_time <  DT.timedelta(seconds=60*10) or int(comment["like_count"])>=10):
+            #if(True):
+                print(comment)
+                filtered_comments.append(comment)
+        print("================================================")
+        print(json.dumps(filtered_comments,indent=2, sort_keys=True))
+        print("===============================================")
+        filteredJson={}
+        filteredJson["data"] = filtered_comments
+        sendData= json.dumps(filteredJson,indent=2, sort_keys=True)
+        byteData = bytes(sendData, 'utf-8')
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.sendto(byteData,("127.0.0.1",1234))
+    except:
+        print("*********************Error***************************")
+    while True:
+        end_time = datetime.now(DT.timezone.utc)
+        if( end_time-start_time > DT.timedelta(seconds=1)):
+            break
